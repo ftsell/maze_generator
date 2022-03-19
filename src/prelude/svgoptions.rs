983fb47393c9f1150f59245584e5550effd21244
langp11 <77873080+langp11@users.noreply.github.com>
2022-03-20 10:20:28 +1100

    Changes as per suggestions

--- src/prelude/svgoptions.rs
+++ src/prelude/svgoptions.rs
@@ -1,13 +1,10 @@
-use crate::prelude::*;
-use std::fmt::{Debug, Display, Formatter};
-
-/// Otions for enerating SVG output
+/// Options for generating SVG output
 #[derive(Debug)]
-pub struct SVGoptions {
+pub struct SvgOptions {
     /// Padding, default: 10
     pub padding: i32,
-    /// Height in pixels - use zero to derive a height based on the padding
-    pub height: i32,
+    /// Height in pixels - use None to derive a height based on the padding and number of cells in the maze
+    pub height: Option<i32>,
     /// Marker size - start and end, default: 2
     pub markersize: i32,
     /// Start marker colour - either a named colour like 'red' or a hex string like '#FF0000', default: "red"
@@ -20,17 +17,17 @@ pub struct SVGoptions {
     pub strokecol: String,
 }
 
-impl SVGoptions {
-    /// Create a new instance from the specified coordinate components
+impl SvgOptions {
+    /// Create a default SvgOptions object
     pub fn new() -> Self {
         Default::default()
     }
 }
 
-impl Default for SVGoptions {
+impl Default for SvgOptions {
     fn default() -> Self {
-        SVGoptions {
-            height: 0,
+        SvgOptions {
+            height: None,
             padding: 10,
             markersize: 2,
             startcol: String::from("red"),
